822e7a3112f3a907fffb6bcef3b8e67f2306440d
Barrantes35 <79799390+Gabriel3555@users.noreply.github.com>
2025-01-04 14:35:28 -0500

    Creating classes and Mapping in JPA

--- src/main/java/gdbv/clinica/models/UserOwner.java
+++ src/main/java/gdbv/clinica/models/UserOwner.java
@@ -4,12 +4,29 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Setter
 @Getter
-@MappedSuperclass
-public class UserOwner extends Person {
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "user_owners", schema = "clinica")
+public abstract class UserOwner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @OneToOne(mappedBy = "owner")
+    private String name;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private LocalDate bornDate;
+
+    protected UserOwner() {
+        bornDate = LocalDate.now();
+    }
+
+    @OneToOne(mappedBy = "owner", cascade = CascadeType.ALL)
     private User user;
-
 }
